f1142bf707b1e839f06a5ec7c70597ef4fe1e23f
arzujala <>
2025-05-27 12:44:55 +0400

    added new one

--- შეფასება 4/script.js
+++ შეფასება 4/script.js
@@ -1,4 +1,38 @@
 const today = new Date();
 const day = today.getDate();
 const month = today.getMonth();
-const year = today.getFullYear
+const year = today.getFullYear();
+
+document.getElementById("dateOutput").innerHTML = `დღევანდელი თარიღია: ${day}/${month}/${year}`;
+  console.log("თარიღი:", day, month, year);
+
+
+//2
+
+let namei = 'nika';
+let upperName = namei.toUpperCase();
+console.log("მაღალ რეგისტრში:", upperName);
+
+//3 
+localStorage.setItem("myText", "ლოკალურ მეხსიერებაში");
+const savedText = localStorage.getItem("myText");
+document.getElementById("localStorageOutput").innerText = savedText;
+
+//4 
+window.onload = function() {
+    alert("გვერდი ჩაიტვირთა წარმატებით!");
+  }
+
+
+  //5
+
+  let count = 1;
+  let interval = setInterval(function() {
+    if (count <= 10) {
+      document.getElementById("counterOutput").innerHTML += count + "<br>";
+      console.log(count);
+      count++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
