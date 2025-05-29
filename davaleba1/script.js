dcbe9bccf5370d8e65ea8bd9b21f1f0fac9afaa5
salome <salomegogokhia738@gmail.com>
2025-05-29 12:47:46 +0400

    .

---
+++ davaleba1/script.js
@@ -0,0 +1,28 @@
+// let number1 = 10;
+// let number2 = 5;
+
+
+// function getSum(a, b) {
+//     return a + b;
+// }
+
+// let result = getSum(number1, number2);
+// console.log("getSum:", result);
+
+// let number1 = 7;
+// let number2 = 3;
+
+// function getSum(a, b) {
+
+// }
+
+// let result = getSum(number1, number2);
+// console.log("ჯამი არის:"), result;
+
+
+
+let number1 = 9;
+let number2 = 5;
+
+let sum = number1 + number2; 
+console.log("ჯამი არის :", sum);
