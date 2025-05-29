02a43d7676bde048e1cf26d253c24f0c809ccfb2
Natia <karamnishvili.natia@gmail.com>
2025-05-29 13:17:43 +0400

    feat: solved problem in 6th folder part 4

--- all_files/6/main.js
+++ all_files/6/main.js
@@ -1,71 +1,69 @@
 // part' 1'
-function returnPromise(){
-return new Promise((resolve) =>{
+function returnPromise() {
+  return new Promise((resolve) => {
     setTimeout(() => {
-        resolve('times up')
+      resolve("times up");
     }, 2000);
-})
+  });
 }
 
-returnPromise().then((result) =>{
-    console.log(result)
-})
-
-
+returnPromise().then((result) => {
+  console.log(result);
+});
 
 //part 2
-async function getResult(){
-    const result = await returnPromise()
-    console.log(result)
-} 
+async function getResult() {
+  const result = await returnPromise();
+  console.log(result);
+}
 
-getResult()
-
-
+getResult();
 
 // part 3
-async function sendData(){
-    const URL = `https://jsonplaceholder.typicode.com/posts`
-    try{
-        const response = await fetch(URL, {
-            method: 'POST',
-             headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                    "userId": 1,
-                    "id": 1,
-                    "title": "sunt aut facere reprehenderit",
-                    "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-            })
-            
-        })
-        const data = await response.json()
-        console.log('success:', data)
-    }
-    catch(err){
-       console.error(err)
-    }
+async function sendData() {
+  const URL = `https://jsonplaceholder.typicode.com/posts`;
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere reprehenderit",
+        body: "quia et suscipit suscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      }),
+    });
+    const data = await response.json();
+    console.log("success:", data);
+  } catch (err) {
+    console.error(err);
+  }
 }
 // sendData()
 
 
-
 // part 4
-async function getData(){
-    const URL = 'https://jsonplaceholder.typicode.com/posts?_limit=5'
-    try{
-        const response = await fetch(URL, {
-            method: 'GET',
-        }
+async function getData() {
+  const URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+  try {
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    const data = await response.json();
 
-        )
-        const data = await response.json()
-        console.log('data:', data)
-    }
-    catch(err){
-      console.log(err)
-    }
+    data.forEach((dataEle) => {
+      const id = dataEle.id;
+      console.log(id);
+
+      const title = dataEle.title;
+      console.log(title);
+    });
+    console.log("data:", data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getData()
+getData();
